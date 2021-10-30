39ec0f3ebfe6305f36385c298b91c1bdb614055e
Roberto <robertodimatteo11@gmail.com>
2021-10-30 16:09:09 +0200

    edddddd

--- main/main.cpp
+++ main/main.cpp
@@ -2,3 +2,18 @@
 // Created by kecco on 30/10/21.
 //
 
+#include "ScreenCapture.h"
+
+using namespace std;
+
+/* driver function to run the application */
+int main()
+{
+    char
+    ScreenCapture video_record(":0.0");
+
+
+    cout<<"\nprogram executed successfully\n";
+
+    return 0;
+}
