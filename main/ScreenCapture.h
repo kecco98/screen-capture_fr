f3f34ab4a4f1c7997f99a868049c11015a74f91f
Roberto <robertodimatteo11@gmail.com>
2021-10-30 15:40:21 +0200

    edddddd

--- main/ScreenCapture.h
+++ main/ScreenCapture.h
@@ -1,2 +1,61 @@
 
 #pragma once
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <cstring>
+#include <math.h>
+#include <string.h>
+
+#define __STDC_CONSTANT_MACROS
+
+//FFMPEG LIBRARIES
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+#include "libavcodec/avfft.h"
+
+#include "libavdevice/avdevice.h"
+
+#include "libavfilter/avfilter.h"
+//#include "../include/avfiltergraph.h"
+//#include "libavfilter/avfiltergraph.h"
+#include "libavfilter/buffersink.h"
+#include "libavfilter/buffersrc.h"
+
+#include "libavformat/avformat.h"
+#include "libavformat/avio.h"
+
+// libav resample
+
+#include "libavutil/opt.h"
+#include "libavutil/common.h"
+#include "libavutil/channel_layout.h"
+#include "libavutil/imgutils.h"
+#include "libavutil/mathematics.h"
+#include "libavutil/samplefmt.h"
+#include "libavutil/time.h"
+#include "libavutil/opt.h"
+#include "libavutil/pixdesc.h"
+#include "libavutil/file.h"
+
+// lib swresample
+
+#include "libswscale/swscale.h"
+
+}
+
+class ScreenCapture
+{
+private:
+
+public:
+
+    ScreenCapture();
+    ~ScreenCapture();
+
+
+};
+
