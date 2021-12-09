29708ff27b53748f818f73496d03018822d20b48
kecco98 <kecco98@outlook.com>
2021-12-09 19:47:03 +0100

    Su mac parte senza errori ma non si sente l'audio

--- main/ScreenCapture.cpp
+++ main/ScreenCapture.cpp
@@ -43,7 +43,7 @@ ScreenCapture::~ScreenCapture(){
         cout<<"\nunable to free avformat context";
         exit(1);
     }*/
-/*
+
     avformat_close_input(&pAudioFormatContext);
     if(pAVFormatContext == nullptr){
         cout<<"Flie close succesfully"<<endl;
@@ -60,412 +60,12 @@ ScreenCapture::~ScreenCapture(){
     } else {
         cerr<<"Error: unable to close the file";
         exit(-1);
-    }*/
+    }
    // avcodec_free_context(&pAVCodecContext); gia fatta
 
     avformat_close_input(&outAVFormatContext);
     //avcodec_free_context(&outAudioCodecContext);
     //avcodec_free_context(&outAVCodecContext);gia fatta
-}
-
-int ScreenCapture::setup(const char* output_file, int width, int height, const char* conc)
-{
-    pAVFormatContext = NULL;
-    options = NULL;
-
-    //Context allocation
-    pAVFormatContext = avformat_alloc_context();
-    //Set screen as input device
-    pAVInputFormat = av_find_input_format("x11grab");
-
-//https://www.titanwolf.org/Network/q/eb46fd58-9cf7-4160-a1a2-a924d8c5639a/y
-    //The distance from the left edge of the screen or desktop
-    //av_dict_set(&options,"offset_x","20",0);
-    //The distance from the top edge of the screen or desktop
-    //av_dict_set(&options,"offset_y","40",0);
-
-    if(av_dict_set( &options,"framerate","30",0 ) < 0)
-    {
-        cout<<"\nerror in setting dictionary value";
-        exit(2);
-    }
-
-
-    if(av_dict_set( &options,"video_size",conc,0 ) < 0)
-    {
-        cout<<"\nerror in setting video size";
-        exit(3);
-    }
-
-    av_dict_set( &options, "preset", "superfast", 0 );
-
-
-
-    if (av_dict_set(&options, "probesize", "60M", 0) < 0) {
-        cerr << "Error in setting probesize value" << endl;
-        exit(-1);
-    }
-    /*if(av_dict_set_int( &options,"height",1000,0 ) < 0)
-      {
-          cout<<"\nerror in setting dictionary value";
-          exit(4);
-      }*/
-
-    if(avformat_open_input(&pAVFormatContext, ":0.0", pAVInputFormat, &options) != 0) { //start= 0.0+x,y punto partenza display
-        cout<<"Error in opening the input device!";
-        exit(1);
-    }
-
-    if(avformat_find_stream_info(pAVFormatContext,NULL) < 0) //da fare forse per il pausa e riprendi
-    {
-        cout<<"\nunable to find the stream information";
-        exit(1);
-    }
-
-    VideoStreamIndx = -1;
-
-    /* find the first video stream index . Also there is an API available to do the below operations */
-    for(int i = 0; i < pAVFormatContext->nb_streams; i++ ) // find video stream posistion/index.
-    {
-        if( pAVFormatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO )
-        {
-            VideoStreamIndx = i;
-            break;
-        }
-
-    }
-
-    if( VideoStreamIndx == -1)
-    {
-        cout<<"\nunable to find the video stream index. (-1)";
-        exit(1);
-    }
-    // Saving video codec
-    pAVCodecContext = pAVFormatContext->streams[VideoStreamIndx]->codec;
-
-
-
-    pAVCodec = avcodec_find_decoder(pAVCodecContext->codec_id);//trova il codec
-    if( pAVCodec == NULL )
-    {
-        cout<<"\nunable to find the decoder";
-        exit(1);
-    }
-
-    /*-- FINE OPEN VIDEO DEVICE --*/
-
-    //Initialize the AVCodecContext to use the given AVCodec. ,copia info codec in ctx
-    if( avcodec_open2(pAVCodecContext , pAVCodec , NULL) < 0 )
-    {
-        cout<<"\nUnable to open the av codec"<<endl;
-        exit(1);
-    }
-
-    //INIT
-
-    avformat_alloc_output_context2(&outAVFormatContext, NULL, NULL, output_file);
-    if (!outAVFormatContext)
-    {
-        cout<<"\nError in allocating av format output context"<<endl;
-        exit(1);
-    }
-
-    output_format = av_guess_format(NULL, output_file ,NULL);
-    if( !output_format )
-    {
-        cout<<"\nerror in guessing the video format. try with correct format";
-        exit(1);
-    }
-
-    /*-- INIZIA OPEN AUDIO DEVICE--*/
-
-    audioOptions=nullptr;
-
-    pAudioFormatContext = nullptr;
-
-    pAudioFormatContext = avformat_alloc_context();
-    if(pAudioFormatContext==nullptr){
-        cerr << "Error: paudiocontext" << endl;
-    }
-    if ( av_dict_set(&audioOptions, "sample_rate", "44100", 0) < 0) {
-        cerr << "Error: cannot set audio sample rate" << endl;
-        exit(-1);
-    }
-
-    if (av_dict_set(&audioOptions, "async", "1", 0) < 0) {
-        cerr << "Error: cannot set audio sample rate" << endl;
-        exit(-1);
-    }
-    //av_dict_set(&audioOptions, "audio_device_number", "0", 0);
-    //av_dict_set(&audioOptions, "thread_queue_size", "4096", 0);
-
-//#if defined linux
-    pAudioInputFormat = av_find_input_format("alsa");
-    /*string deviceName;
-    if(deviceName == "") deviceName = "default"; */
-   // pAudioInputFormat =av_find_input_format("pulseaudio");
-
-
-    if (avformat_open_input(&pAudioFormatContext, "default", pAudioInputFormat, &audioOptions) != 0) {
-        cerr << "Error in opening input device (audio)" << endl;
-        exit(-1);
-    }
-//#endif
-
-
-/*#if defined _WIN32
-    audioInputFormat = av_find_input_format("dshow");
-    value = avformat_open_input(&inAudioFormatContext, "audio=Microfono (Realtek(R) Audio)", audioInputFormat, &audioOptions);
-    if (value != 0) {
-        cerr << "Error in opening input device (audio)" << endl;
-        exit(-1);
-    }
-#endif*/
-
-
-    if (avformat_find_stream_info(pAudioFormatContext, nullptr) != 0) {
-        cerr << "Error: cannot find the audio stream information" << endl;
-        exit(-1);
-    }
-
-    for (int i = 0; i < pAudioFormatContext->nb_streams; i++) {
-        if (pAudioFormatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
-            audioStreamIndx = i;
-            break;
-        }
-    }
-    if (audioStreamIndx == -1) {
-        cerr << "Error: unable to find audio stream index" << endl;
-        exit(-2);
-    }
-
-    //QUI
-
-    video_st = avformat_new_stream(outAVFormatContext ,NULL);
-    if( !video_st )
-    {
-        cout<<"\nerror in creating a av format new stream";
-        exit(1);
-    }
-
-    outAVCodec=nullptr;
-
-    outAVCodecContext = avcodec_alloc_context3(outAVCodec);//outAvCodec nullo cosi in auto sceflie il settaggio migliore per il ctx
-    if( !outAVCodecContext )
-    {
-        cout<<"\nerror in allocating the codec contexts";
-        exit(1);
-    }
-
-    /* set property of the video file */
-    /*outAVCodecContext = video_st->codec;
-    //avcodec_parameters_to_context(outAVCodecContext, video_st->codecpar);
-
-    outAVCodecContext->codec_id = AV_CODEC_ID_MPEG4;// AV_CODEC_ID_MPEG4; // AV_CODEC_ID_H264 // AV_CODEC_ID_MPEG1VIDEO
-    outAVCodecContext->codec_type = AVMEDIA_TYPE_VIDEO;
-    outAVCodecContext->pix_fmt  = AV_PIX_FMT_YUV420P;
-    outAVCodecContext->bit_rate = 800000; // 2500000
-    outAVCodecContext->width = width;
-    outAVCodecContext->height = height;
-    outAVCodecContext->gop_size = 3;
-    outAVCodecContext->max_b_frames = 2;
-    outAVCodecContext->time_base.num = 1;
-    outAVCodecContext->time_base.den = 30;*/
-
-    outAVCodecContext = video_st->codec;
-    outAVCodecContext->codec_id = AV_CODEC_ID_MPEG4;
-    outAVCodecContext->codec_type = AVMEDIA_TYPE_VIDEO;
-    outAVCodecContext->pix_fmt = AV_PIX_FMT_YUV420P;
-    outAVCodecContext->bit_rate = 10000000;
-    outAVCodecContext->width = width;
-    outAVCodecContext->height = height;
-    outAVCodecContext->gop_size = 10;
-    outAVCodecContext->global_quality = 500;
-    outAVCodecContext->max_b_frames = 2;
-    outAVCodecContext->time_base.num = 1;
-    outAVCodecContext->time_base.den = 30;
-    outAVCodecContext->bit_rate_tolerance = 400000;
-
-    if (outAVCodecContext->codec_id == AV_CODEC_ID_H264) { //copiato da vedere a cosa serve
-        av_opt_set(outAVCodecContext->priv_data, "preset", "slow", 0);
-    }
-
-
-    /* char *wid; Display *dpy; Window w;
-      int width2, height2, snum;
-      dpy = XOpenDisplay(0);
-
-      snum = DefaultScreen(dpy);
-      width2 = DisplayWidth(dpy, snum);
-      height2 = DisplayHeight(dpy, snum);
-      printf("display size is %d x %d\n", width2, height2);*/
-
-    //Per un altra codifica!!
-    /*   if (codec_id == AV_CODEC_ID_H264)
-       {
-           av_opt_set(outAVCodecContext->priv_data, "preset", "slow", 0);
-       }*/
-
-    outAVCodec = avcodec_find_encoder(AV_CODEC_ID_MPEG4);//prenderlo da sopra!!!
-    if( !outAVCodec )
-    {
-        cout<<"\nError in finding the av codecs. try again with correct codec"<<endl;
-        exit(1);
-    }
-
-    /* Some container formats (like MP4) require global headers to be present
-     Mark the encoder so that it behaves accordingly. */
-
-    if ( outAVFormatContext->oformat->flags & AVFMT_GLOBALHEADER)
-    {
-        outAVCodecContext->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
-    }
-
-
-    if( avcodec_open2(outAVCodecContext, outAVCodec, NULL) < 0) //inizializza out context
-    {
-        cout<<"\nerror in opening the avcodec";
-        exit(1);
-    }
-    avcodec_parameters_from_context(outAVFormatContext->streams[VideoStreamIndx]->codecpar, outAVCodecContext);
-
-    AVCodecParameters* params = pAudioFormatContext->streams[audioStreamIndx]->codecpar;
-
-
-
-    pAudioCodec = avcodec_find_decoder(params->codec_id);
-    if (pAudioCodec == nullptr) {
-        cerr << "Error: cannot find the audio decoder" << endl;
-        exit(-1);
-    }
-
-    pAudioCodecContext = avcodec_alloc_context3(pAudioCodec);
-    if (avcodec_parameters_to_context(pAudioCodecContext, params) < 0) {
-        cout << "Cannot create codec context for audio input" << endl;
-    }
-
-    if (avcodec_open2(pAudioCodecContext, pAudioCodec, nullptr) < 0) {
-        cerr << "Error: cannot open the input audio codec" << endl;
-        exit(-1);
-    }
-
-    //Generate audio stream
-    outAudioCodecContext = nullptr;
-    outAudioCodec = nullptr;
-    int i;
-
-    AVStream* audio_st = avformat_new_stream(outAVFormatContext, nullptr);
-    if (audio_st == nullptr) {
-        cerr << "Error: cannot create audio stream" << endl;
-        exit(1);
-    }
-
-    outAudioCodec = avcodec_find_encoder(AV_CODEC_ID_AAC);
-    if (outAudioCodec == nullptr) {
-        cerr << "Error: cannot find requested encoder" << endl;
-        exit(1);
-    }
-
-    outAudioCodecContext = avcodec_alloc_context3(outAudioCodec);
-    if (outAudioCodecContext == nullptr) {
-        cerr << "Error: cannot create related VideoCodecContext" << endl;
-        exit(1);
-    }
-
-    if (outAudioCodec->supported_samplerates) {
-        outAudioCodecContext->sample_rate = outAudioCodec->supported_samplerates[0];
-        for (i = 0; outAudioCodec->supported_samplerates[i]; i++) {
-            if (outAudioCodec->supported_samplerates[i] == pAudioCodecContext->sample_rate)
-                outAudioCodecContext->sample_rate = pAudioCodecContext->sample_rate;
-        }
-    }
-    outAudioCodecContext->codec_id = AV_CODEC_ID_AAC;
-    outAudioCodecContext->sample_fmt = outAudioCodec->sample_fmts ? outAudioCodec->sample_fmts[0] : AV_SAMPLE_FMT_FLTP;
-    outAudioCodecContext->channels = pAudioCodecContext->channels;
-    outAudioCodecContext->channel_layout = av_get_default_channel_layout(outAudioCodecContext->channels);
-    outAudioCodecContext->bit_rate = 96000;
-    outAudioCodecContext->time_base = { 1, pAudioCodecContext->sample_rate };
-
-    outAudioCodecContext->strict_std_compliance = FF_COMPLIANCE_EXPERIMENTAL;
-
-    if ((outAVFormatContext)->oformat->flags & AVFMT_GLOBALHEADER) {
-        outAudioCodecContext->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
-    }
-
-    if (avcodec_open2(outAudioCodecContext, outAudioCodec, nullptr) < 0) {
-        cerr << "error in opening the avcodec" << endl;
-        exit(1);
-    }
-
-    //find a free stream index
-    for (i = 0; i < outAVFormatContext->nb_streams; i++) {
-        if (outAVFormatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_UNKNOWN) {
-            outAudioStreamIndex = i;
-        }
-    }
-    if (outAudioStreamIndex < 0) {
-        cerr << "Error: cannot find a free stream for audio on the output" << endl;
-        exit(1);
-    }
-
-   avcodec_parameters_from_context(outAVFormatContext->streams[outAudioStreamIndex]->codecpar, outAudioCodecContext);
-
-    //da qui unificare
-    /* create empty video file */
-    if ( !(outAVFormatContext->flags & AVFMT_NOFILE) )
-    {
-        cout<<output_file<<"  file ouy\n ";
-        if( avio_open2(&outAVFormatContext->pb , output_file , AVIO_FLAG_WRITE ,NULL, NULL) < 0 )
-        {
-            cout<<"\nerror in creating the video file";
-            exit(1);
-        }
-    }
-
-    if(!outAVFormatContext->nb_streams)
-    {
-        cout<<"\noutput file dose not contain any stream";
-        exit(1);
-    }
-
-    /* imp: mp4 container or some advanced container file required header information*/
-    if(avformat_write_header(outAVFormatContext , &options) < 0)
-    {
-        cout<<"\nerror in writing the header context";
-        exit(1);
-    }
-
-    cout<<"\n\nOutput file information :\n\n";
-    av_dump_format(outAVFormatContext , 0 ,output_file ,1);
-
-
-    pAVPacket = (AVPacket *)av_malloc(sizeof(AVPacket));
-    av_init_packet(pAVPacket);
-
-
-    pAVFrame = av_frame_alloc();
-    //pAVFrame->width=width;
-    // pAVFrame->height=height;
-    // pAVFrame->format=AV_PIX_FMT_YUV420P;
-
-    if( !pAVFrame )
-    {
-        cout<<"\nunable to release the avframe resources";
-        exit(1);
-    }
-
-    outFrame = av_frame_alloc();//Allocate an AVFrame and set its fields to default values.
-    outFrame->width=width;
-    outFrame->height=height;
-    outFrame->format=AV_CODEC_ID_MPEG4;
-
-    if( !outFrame )
-    {
-        cout<<"\nunable to release the avframe resources for outframe";
-        exit(1);
-    }
-    avformat_close_input(&pAudioFormatContext);
-    return 10;
 }
 
 int ScreenCapture::genMenu() {
@@ -517,16 +117,15 @@ int ScreenCapture::start() {
 
 
     videoStream = new std::thread(&ScreenCapture::startVideoRecording,this);
-    //audioStream = new std::thread(&ScreenCapture::startAudioRecording,this);
+    audioStream = new std::thread(&ScreenCapture::startAudioRecording,this);
 
     videoStream->join();
-    //audioStream->join();
+    audioStream->join();
 
     menu->join();
   return 1;
 
 }
-
 
 int ScreenCapture::startVideoRecording() {
     //https://stackoverflow.com/questions/54338342/ffmpeg-rgb-to-yuv420p-warning-data-is-not-aligned-this-can-lead-to-a-speedlo
@@ -744,7 +343,6 @@ int ScreenCapture::startAudioRecording() {
     }
     int ii=0;
 
-    avformat_open_input(&pAudioFormatContext, "default", pAudioInputFormat, &audioOptions);
 
     while ( running /*av_read_frame(pAudioFormatContext, inPacket) >= 0 *//*&& inPacket->stream_index == audioStreamIndx*/) {
 
@@ -756,7 +354,21 @@ int ScreenCapture::startAudioRecording() {
            // avcodec_flush_buffers(m_codec_ctx.get());
             avformat_close_input(&pAudioFormatContext);
         cv_a.wait(lp, [this]() { return !pause; });
-       avformat_open_input(&pAudioFormatContext, "default", pAudioInputFormat, &audioOptions);
+        #if defined linux
+                    avformat_open_input(&pAudioFormatContext, "default", pAudioInputFormat, &audioOptions);
+        #endif
+
+        #if defined _WIN32
+
+                    avformat_open_input(&inAudioFormatContext, "audio=Microfono (Realtek(R) Audio)", audioInputFormat, &audioOptions);
+
+        #else
+                    if (av_dict_set(&audioOptions, "audio_device_index", "0", 0) < 0) {
+                        cerr << "Error: cannot set audio device number" << endl;
+                        exit(-1);
+                    }
+                    avformat_open_input(&pAudioFormatContext, "", pAudioInputFormat, &audioOptions);
+        #endif
 
     }
         if( av_read_frame(pAudioFormatContext, inPacket) <0) break;
@@ -848,6 +460,7 @@ int ScreenCapture::startAudioRecording() {
     }
     swr_free(&resampleContext);
 }
+
 int ScreenCapture::init_fifo()
 {
     /* Create the FIFO buffer based on the specified output sample format. */
@@ -907,15 +520,6 @@ int ScreenCapture::openInputVideo() {
     pAVFormatContext = avformat_alloc_context();
     //Set screen as input device
 
-#ifdef _WIN32
-    pAVInputFormat = av_find_input_format("gdigrab");
-    if (avformat_open_input(&pAVFormatContext, "desktop", pAVInputFormat, &options) != 0) {
-        cerr << "Couldn't open input stream" << endl;
-        exit(-1);
-    }
-
-#elif defined linux
-
     if(av_dict_set( &options,"framerate","30",0 ) < 0)
     {
         cout<<"\nerror in setting dictionary value";
@@ -934,6 +538,15 @@ int ScreenCapture::openInputVideo() {
         cerr << "Error in setting probesize value" << endl;
         exit(-1);
     }
+
+#ifdef _WIN32
+    pAVInputFormat = av_find_input_format("gdigrab");
+    if (avformat_open_input(&pAVFormatContext, "desktop", pAVInputFormat, &options) != 0) {
+        cerr << "Couldn't open input stream" << endl;
+        exit(-1);
+    }
+
+#elif defined linux
 
     pAVInputFormat = av_find_input_format("x11grab");
 
@@ -1120,8 +733,6 @@ int ScreenCapture::openInputVideo() {
         exit(1);
     }
 
-    streamTrail();
-
 
     return 0;
 }
@@ -1147,7 +758,7 @@ int ScreenCapture::openInputAudio() {
         cerr << "Error: cannot set audio sample rate" << endl;
         exit(-1);
     }
-    //av_dict_set(&audioOptions, "audio_device_number", "0", 0);
+
     //av_dict_set(&audioOptions, "thread_queue_size", "4096", 0);
 
 #if defined linux
@@ -1170,6 +781,20 @@ int ScreenCapture::openInputAudio() {
         cerr << "Error in opening input device (audio)" << endl;
         exit(-1);
     }
+#else
+
+    if (av_dict_set(&audioOptions, "audio_device_index", "0", 0) < 0) {
+        cerr << "Error: cannot set audio device number" << endl;
+        exit(-1);
+    }
+
+    pAudioInputFormat = av_find_input_format("avfoundation");
+
+    if (avformat_open_input(&pAudioFormatContext, "", pAudioInputFormat, &audioOptions) != 0) {
+        cerr << "Error in opening input device (audio)" << endl;
+        exit(-1);
+    }
+
 #endif
 
     if (avformat_find_stream_info(pAudioFormatContext, nullptr) != 0) {
@@ -1269,8 +894,6 @@ int ScreenCapture::openInputAudio() {
 
     avcodec_parameters_from_context(outAVFormatContext->streams[outAudioStreamIndex]->codecpar, outAudioCodecContext);
 
-    streamTrail();
-
     return 0;
 }
 
@@ -1286,8 +909,8 @@ int ScreenCapture::openInput(int widthi, int heighti,const char* outputi,bool au
     output=outputi;
 
     openInputVideo();
-   // openInputAudio();
-    //streamTrail();
+    openInputAudio();
+    streamTrail();
 
 
 
